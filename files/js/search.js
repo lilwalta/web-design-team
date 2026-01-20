90d18614cfb91f1e34e143fb29a7836ee272e02d
Wyatt Lang <104957732+lilwalta@users.noreply.github.com>
2026-01-20 08:57:38 -0800

    Create search.js

---
+++ files/js/search.js
@@ -0,0 +1,52 @@
+const SEARCH_INDEX = [
+  {
+    title: "Anxiety Disorders",
+    type: "Disorder",
+    keywords: ["anxiety", "panic", "worry", "phobia", "stress"],
+    url: "/disorders/anxiety/"
+  },
+  {
+    title: "Depression",
+    type: "Disorder",
+    keywords: ["depression", "sad", "hopeless", "mood"],
+    url: "/disorders/depression/"
+  },
+  {
+    title: "PTSD & Trauma",
+    type: "Disorder",
+    keywords: ["ptsd", "trauma", "flashbacks"],
+    url: "/disorders/ptsd/"
+  },
+  {
+    title: "Eating Disorders",
+    type: "Disorder",
+    keywords: ["eating", "anorexia", "bulimia"],
+    url: "/disorders/eating/"
+  },
+  {
+    title: "Schizophrenia",
+    type: "Disorder",
+    keywords: ["schizo", "psychosis", "hallucinations"],
+    url: "/disorders/schizophrenia/"
+  },
+  {
+    title: "Substance Use Disorders",
+    type: "Disorder",
+    keywords: ["substance", "addiction", "drugs", "alcohol"],
+    url: "/disorders/substance/"
+  },
+
+  // Articles / resources
+  {
+    title: "Mental Health Resources",
+    type: "Resources",
+    keywords: ["resources", "help", "support"],
+    url: "/resources/"
+  },
+  {
+    title: "Take Action",
+    type: "Action",
+    keywords: ["action", "volunteer", "pledge"],
+    url: "/action/"
+  }
+];

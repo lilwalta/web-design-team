ac921892a1728ec843cf41641ea20b48f29e564b
Wyatt Lang <104957732+lilwalta@users.noreply.github.com>
2026-02-26 08:04:31 -0800

    Create search-data.js

---
+++ files/js/search-data.js
@@ -0,0 +1,68 @@
+const SEARCH_INDEX = [
+  {
+    title: "Depression",
+    url: "/disorders/depression/",
+    type: "Disorder"
+  },
+  {
+    title: "Anxiety",
+    url: "/disorders/anxiety/",
+    type: "Disorder"
+  },
+  {
+    title: "PTSD",
+    url: "/disorders/ptsd/",
+    type: "Disorder"
+  },
+  {
+    title: "ADHD",
+    url: "/disorders/adhd/",
+    type: "Disorder"
+  },
+  {
+    title: "Bipolar",
+    url: "/disorders/bipolar/",
+    type: "Disorder"
+  },
+  {
+    title: "Eating Disorder",
+    url: "/disorders/eating/",
+    type: "Disorder"
+  },
+  {
+    title: "OCD",
+    url: "/disorders/ocd/",
+    type: "Disorder"
+  },
+  {
+    title: "Schizophrenia",
+    url: "/disorders/schizophrenia/",
+    type: "Disorder"
+  },
+  {
+    title: "Substance Use",
+    url: "/disorders/substance-use/",
+    type: "Disorder"
+  },
+  {
+    title: "Troubling AI Relationships",
+    url: "/news/ai-relationships/",
+    type: "Article"
+  },
+  {
+    title: "Annual Mental Health Walk",
+    url: "/news/annual-mental-health-walk-returns/",
+    type: "Event"
+  },
+  {
+    title: "Community Education",
+    url: "/news/community-education/",
+    type: "Event"
+  },
+  {
+    title: "Thrivwell Online Support",
+    url: "/news/thrivewell-online-support/",
+    type: "Event"
+  },
+  
+];
